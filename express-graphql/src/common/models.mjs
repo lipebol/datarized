2e0e69775da145c5f070ad5cbb364b038e58ab83
lipebol <lipe_bol5@hotmail.com>
2025-12-19 14:31:32 +0000

    changed 'id' name and add '__typename'

--- express-graphql/src/common/models.mjs
+++ express-graphql/src/common/models.mjs
@@ -4,7 +4,7 @@ import { mongooseAddons, mongooseSpotifEx } from './instances.mjs'
 export const spotifExGenres = mongooseSpotifEx.model(
     'spotifExGenres', new mongoose.Schema(
         {
-            id: { type: mongoose.ObjectId, required: true },
+            _id: { type: mongoose.ObjectId, required: true },
             name: { type: String, required: true },
             about: { type: String, required: true }
         }, mongooseAddons
@@ -12,64 +12,78 @@ export const spotifExGenres = mongooseSpotifEx.model(
 )
 
 export const spotifExArtists = mongooseSpotifEx.model(
-    'spotifExArtists', new mongoose.Schema(
-        {
-            id: { type: mongoose.ObjectId, required: true },
-            artistid: { type: String, required: true },
-            name: { type: String, required: true },
-            profile: { type: String, required: true },
-            followers: { type: Number, required: true },
-            images: [
+    'spotifExArtists', (
+        () => {
+            const schema = new mongoose.Schema(
                 {
-                    url: { type: String, required: true },
-                    width: { type: Number, required: true },
-                    height: { type: Number, required: true },
-                }
-            ],
-            genres: [{ type: mongoose.ObjectId, ref: 'spotifExGenres' }]
-        }, mongooseAddons
-    ), 'artists'
+                    _id: { type: mongoose.ObjectId, required: true },
+                    artistid: { type: String, required: true },
+                    name: { type: String, required: true },
+                    profile: { type: String, required: true },
+                    followers: { type: Number, required: true },
+                    images: [
+                        {
+                            url: { type: String, required: true },
+                            width: { type: Number, required: true },
+                            height: { type: Number, required: true },
+                        }
+                    ],
+                    genres: [{ type: mongoose.ObjectId, ref: 'spotifExGenres' }]
+                }, mongooseAddons
+            )
+            schema.virtual('__typename').get(() => { return 'spotifExArtistFields' })
+            return schema
+        }
+    )(), 'artists'
 )
 
 
 export const spotifExAlbums = mongooseSpotifEx.model(
-    'spotifExAlbums', new mongoose.Schema(
-        {
-            id: { type: mongoose.ObjectId, required: true },
-            albumid: { type: String, required: true },
-            name: { type: String, required: true },
-            album_type: { type: String, required: true },
-            release_date: { type: String, required: true },
-            available_markets: [{ type: String, required: true }],
-            external_url: { type: String, required: true },
-            images: [
+    'spotifExAlbums', (
+        () => {
+            const schema = new mongoose.Schema(
                 {
-                    url: { type: String, required: true },
-                    width: { type: Number, required: true },
-                    height: { type: Number, required: true },
-                }
-            ],
-            total_tracks: { type: Number, required: true },
-            copyrights: [
-                {
-                    text: { type: String, required: true },
-                    type: { type: String, required: true },
-                }
-            ],
-            label: { type: String, required: true }
-        }, mongooseAddons
-    ), 'albums'
+                    _id: { type: mongoose.ObjectId, required: true },
+                    albumid: { type: String, required: true },
+                    name: { type: String, required: true },
+                    album_type: { type: String, required: true },
+                    release_date: { type: String, required: true },
+                    available_markets: [{ type: String, required: true }],
+                    no_available_markets: [{ type: String, required: true }],
+                    external_url: { type: String, required: true },
+                    images: [
+                        {
+                            url: { type: String, required: true },
+                            width: { type: Number, required: true },
+                            height: { type: Number, required: true },
+                        }
+                    ],
+                    total_tracks: { type: Number, required: true },
+                    copyrights: [
+                        {
+                            text: { type: String, required: true },
+                            type: { type: String, required: true },
+                        }
+                    ],
+                    label: { type: String, required: true }
+                }, mongooseAddons
+            )
+            schema.virtual('__typename').get(() => { return 'spotifExAlbumFields' })
+            return schema
+        }
+    )(), 'albums'
 )
 
 
 export const spotifExTracks = mongooseSpotifEx.model(
     'spotifExTracks', new mongoose.Schema(
         {
-            id: { type: mongoose.ObjectId, required: true },
+            _id: { type: mongoose.ObjectId, required: true },
             trackid: { type: String, required: true },
             name: { type: String, required: true },
             album: { type: mongoose.ObjectId, ref: 'spotifExAlbums' },
             artists: [{ type: mongoose.ObjectId, ref: 'spotifExArtists' }],
+            url: { type: String, required: true },
             duration_ms: { type: Number, required: true },
             popularity: { type: Number, required: true },
             explicit: { type: Boolean, required: true },
@@ -83,7 +97,7 @@ export const spotifExTracks = mongooseSpotifEx.model(
 export const spotifExDaylists = mongooseSpotifEx.model(
     'spotifExDaylists', new mongoose.Schema(
         {
-            id: { type: mongoose.ObjectId, required: true },
+            _id: { type: mongoose.ObjectId, required: true },
             track: { type: mongoose.ObjectId, ref: 'spotifExTracks' },
             date: { type: String, required: true },
             listen: { type: Number, required: true }
