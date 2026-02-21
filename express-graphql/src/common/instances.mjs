abd7ee2c8431b86db212087bdd354d70584e7152
lipebol <lipe_bol5@hotmail.com>
2026-02-21 14:44:34 +0000

    add 's3'

--- express-graphql/src/common/instances.mjs
+++ express-graphql/src/common/instances.mjs
@@ -1,9 +1,9 @@
 import { cryptHandler } from './handlers/crypt.mjs'
-import { mongoose } from 'mongoose'
-import { Sequelize } from 'sequelize'
 
-const [postgresql, mongodb] = ['postgresql-uri.jwe','mongodb-uri.jwe']
+const [postgresql, mongodb, s3] = ['postgresql-uri.jwe','mongodb-uri.jwe','s3.jwe']
 // ========================================= mongoose =========================================
+import { mongoose } from 'mongoose'
+
 mongoose.set(
     'debug', (collection, method, query, agreggate, options) => {
         console.log(
@@ -23,5 +23,19 @@ export const mongooseSpotifEx = mongoose.createConnection(cryptHandler(mongodb))
 
 
 // ========================================= Sequelize =========================================
+import { Sequelize } from 'sequelize'
 
 export const sequelizeConnect = new Sequelize(cryptHandler(postgresql), { dialect: 'postgres' })
+
+
+// ====================================== Object Storage =======================================
+import { S3Client } from '@aws-sdk/client-s3'
+
+const [s3endpoint, access_key, secret_key] = cryptHandler(s3).split(' ')
+
+export const s3connect = new S3Client(
+    {
+        region: 'us-east-1', endpoint: s3endpoint, forcePathStyle: true,
+        credentials: { accessKeyId: access_key, secretAccessKey: secret_key }
+    }
+)
